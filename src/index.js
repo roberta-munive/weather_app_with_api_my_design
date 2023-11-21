070ff08df4a7b592cc954ababae3196e7009b68d
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2023-11-21 11:33:57 -0600

    integrated weather api into the app and display current temperature and city with live data from the weather api

--- src/index.js
+++ src/index.js
@@ -9,19 +9,40 @@ function capitalizeFirstLetterOfWordsInString(str) {
 }
 
 function displayCityName(city) {
-  city = capitalizeFirstLetterOfWordsInString(city);
+  // city = capitalizeFirstLetterOfWordsInString(city);
   let currentCityLocator = document.querySelector("#current-city");
   currentCityLocator.innerHTML = city;
 }
 
-function getCurrentWeatherConditions(event) {
+function displayCurrentWeatherConditions(response) {
+  let city = response.data.city;
+  displayCityName(city);
+
+  let currentTemperature = response.data.temperature.current;
+  currentTemperature = Math.round(currentTemperature);
+  let currentTemperatureLocator = document.querySelector(
+    "#current-temperature"
+  );
+  currentTemperatureLocator.innerHTML = currentTemperature;
+}
+
+function getCity(event) {
   event.preventDefault();
 
   let searchBarInputLocator = document.querySelector("#search-bar-input");
   let city = searchBarInputLocator.value;
-  displayCityName(city);
+  getCurrentWeatherConditions(city);
+}
+
+function getCurrentWeatherConditions(city) {
+  let unit = "imperial";
+  let apiKey = "cf14b4c0f0c0d7a973ee3b4e430t2bo5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(displayCurrentWeatherConditions);
 }
 
 let searchBarLocator = document.querySelector("#search-bar");
 
-searchBarLocator.addEventListener("submit", getCurrentWeatherConditions);
+searchBarLocator.addEventListener("submit", getCity);
+
+// add default city display of current conditions
